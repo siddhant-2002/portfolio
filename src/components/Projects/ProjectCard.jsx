4995bd593c264c38c61790ffea38315e2887aa33
siddhant-2002 <siddeshmukh711@gmail.com>
2024-10-04 19:11:42 +0530

    new changes

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,34 +4,37 @@ import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
+    project: { title, imageSrc, description, skills, demo },
 }) => {
-  return (
-    <div className={styles.container}>
-      <img
-        src={getImageUrl(imageSrc)}
-        alt={`Image of ${title}`}
-        className={styles.image}
-      />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
-      </ul>
-      <div className={styles.links}>
-        <span href={demo} className={styles.link}>
-          Demo
-        </span>
-        <span href={source} className={styles.link}>
-          Source
-        </span>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.div1}>
+                <img
+                    src={getImageUrl(imageSrc)}
+                    alt={`Image of ${title}`}
+                    className={styles.image}
+                />
+                <h3 className={styles.title}>{title}</h3>
+            </div>
+            <div className={styles.div2}>
+
+                <p className={styles.description}>{description}</p>
+                <ul className={styles.skills}>
+                    {skills.map((skill, id) => {
+                        return (
+                            <li key={id} className={styles.skill}>
+                                {skill}
+                            </li>
+                        );
+                    })}
+                </ul>
+                <div className={styles.links}>
+                    <span href={demo} className={styles.link}>
+                        Demo
+                    </span>
+
+                </div>
+            </div>
+        </div>
+    );
 };
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,19 +1,50 @@
+// import React from "react";
+
+// import styles from "./Projects.module.css";
+
+// import projects from "../../data/projects.json";
+// import { ProjectCard } from "./ProjectCard";
+
+// export const Projects = () => {
+//   return (
+//     <section className={styles.container} id="projects">
+//       <h2 className={styles.title}>Projects</h2>
+//       <div className={styles.projects}>
+//         {projects.map((project, id) => {
+//           return <ProjectCard key={id} project={project} />;
+//         })}
+//       </div>
+//     </section>
+//   );
+// };
+
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import styles from "./Projects.module.css";
-
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
-      <div className={styles.projects}>
-        {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
-        })}
-      </div>
+      <Slider {...settings} className={styles.projects}>
+        {projects.map((project, id) => (
+          <ProjectCard key={id} project={project} />
+        ))}
+      </Slider>
     </section>
   );
 };
+
