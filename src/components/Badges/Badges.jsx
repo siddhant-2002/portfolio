75bafbc2321f6209bb5a92002e33e6695af6a60d
siddhant-2002 <siddeshmukh711@gmail.com>
2024-10-17 14:54:09 +0530

    new

---
+++ src/components/Badges/Badges.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import styles from "./Badges.module.css"
+
+ export const Badges = () => {
+  return (
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Skills</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {skills.map((skill, id) => {
+            return (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  )
+}
+
